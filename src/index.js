97b76bc2b62ca5707ae030d74a94344ee9e9b11e
Dima Fedin <fedinamid@gmail.com>
2020-02-02 01:43:10 +0300

    add Procfile and start scripts

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const URL = "https://ultralord-challange-bot.herokuapp.com/";
 
 const bot = new Telegraf(ACCESS_TOKEN);
 bot.telegram.setWebhook(`${URL}/bot${ACCESS_TOKEN}`);
-bot.startWebhook(`/bot${ACCESS_TOKEN}`, PORT);
+bot.startWebhook(`/bot${ACCESS_TOKEN}`, null, PORT);
 
 bot.use(async (ctx, next) => {
   const start = new Date();
