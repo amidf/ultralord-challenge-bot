2fc3fd547719f41712dee067daafc4ba8e158aa1
Dima Fedin <dima.groot.04@gmail.com>
2020-02-02 00:54:29 +0300

    add webhook

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const Telegraf = require("telegraf");
 
 const ACCESS_TOKEN = "911918404:AAFIc-oTTDF5-OrK_UqdQ5sRGnxnYovy7N0";
+const PORT = process.env.PORT || 3000;
+const URL = "https://ultralord-challange-bot.herokuapp.com/";
 
 const bot = new Telegraf(ACCESS_TOKEN);
+bot.telegram.setWebhook(`${URL}/bot${ACCESS_TOKEN}`);
+bot.startWebhook(`/bot${ACCESS_TOKEN}`, PORT);
 
 bot.use(async (ctx, next) => {
   const start = new Date();
